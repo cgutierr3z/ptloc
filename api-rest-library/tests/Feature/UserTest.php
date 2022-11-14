7c897cae997268fdb170350038d1d4778a2165db
Carlos Gutierrez <cgutierrez@utp.edu.co>
2022-11-14 00:11:28 -0500

    basic test to login and singup api routes

--- api-rest-library/tests/Feature/UserTest.php
+++ api-rest-library/tests/Feature/UserTest.php
@@ -19,4 +19,28 @@ class UserTest extends TestCase
             ->assertStatus(200)
             ->assertSee("Action Test on UserController");
     }
+
+    /**
+     * Feature test for users sing-up.
+     *
+     * @test
+     */
+    public function it_loads_users_singup_page()
+    {
+        $this->post('/api/sing-up')
+            ->assertStatus(200)
+            ->assertSee("Action SingUp on UserController");
+    }
+
+    /**
+     * Feature test for users sing-up.
+     *
+     * @test
+     */
+    public function it_loads_users_login_page()
+    {
+        $this->post('/api/login')
+            ->assertStatus(200)
+            ->assertSee("Action Login on UserController");
+    }
 }
